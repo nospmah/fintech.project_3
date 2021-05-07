1bcdb870cc50ff9fa962aab263d788c068770d9e
Brian Hampson <hampson.brian@gmail.com>
2021-05-06 22:02:52 -0700

    QR code modal, menu refactoring

--- public/js/dapp.js
+++ public/js/dapp.js
@@ -21,6 +21,8 @@ const dApp = {
 
     const fetchMetadata = (reference_uri) => fetch(`https://gateway.pinata.cloud/ipfs/${reference_uri.replace("ipfs://", "")}`, { mode: "cors" }).then((resp) => resp.json());
     
+    this.generateQrCode();
+
     this.affiliateGymCount = await this.blockFitterContract.methods.getAffiliateGymCount().call();
     this.affiliateGyms = [];
 
@@ -65,29 +67,39 @@ const dApp = {
 
   render: async function() {    
     
-    // refresh variables
+    console.log(`DEBUG: dapp.render`);
+
+    // Init data
     await this.initData();
 
-    let acctAddress = $("#owner").text(`${JSON.stringify(this.accounts[0]).replace(/["']/g, "")}`);
-    
-    console.log(`DEBUG: dapp.render - Account address: ${acctAddress}`);
+    // Get account address display string
+    const owner_address = JSON.stringify(this.accounts[0]).replace(/["']/g, "");
+    $("#owner").text(`${owner_address}`);
+    $("#qr-code-modal-title").text(`${owner_address}`);    
 
     // Clear affiliate gyms
     $("#affiliate-gym-container").html("");
 
+    // Render cards for each affiliate gym
     this.affiliateGyms.forEach((gym) => {
+      
+      const hash = gym.uri.replace("ipfs://","");
+      const uri = `https://ipfs.io/ipfs/${hash}`;
+      
       $("#affiliate-gym-container").append(
         `
-        <div class="card" style="width: 18rem;">
+        <div class="card w-25">
           <img src="../images/box_2.png" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">${gym.name} (${gym.id})</h5>
-            <h7 class="card-title">${gym.description}</h7>
-            <a href="${gym.uri}" target="_blank" class="card-text">IPFS link</a>
-            <a href="#" class="btn btn-primary" data-id="${gym.id}" onclick="dApp.deactivateAffiliateGym(${gym.id})">Deactivate</a>            
+            <h3 class="card-title">${gym.name}</h3>
+            <p class="card-text">Id: ${gym.id}</p>
+            <p class="card-text">${gym.description}</p>
+            <p class="card-link"><a href="${uri}" target="_blank">Details</a></p>
+            <p><a href="#" class="btn btn-primary" data-id="${gym.id}" onclick="dApp.deactivateAffiliateGym(${gym.id})">Deactivate</a></p>
           </div>
         </div>
-        `);
+        `
+      );
     });
   },
 
@@ -103,9 +115,12 @@ const dApp = {
       const judge_address = $("#reg-aff-judge-address").val();
       // const pinata_api_key = $("#reg-aff-pinata-api-key").val();
       // const pinata_secret_api_key = $("#reg-aff-pinata-api-secret").val();
+      
+      // TODO - remove
       const pinata_api_key = "a190d395a2bde42df710";
       const pinata_secret_api_key = "3a910879529b43f3b1cfd0162920c7ed0aa5693f9887995d8d81e815c8a93413";
 
+      // Required data
       if (!name || !payable_address) {
         alert('Missing data!');
         return;
@@ -161,7 +176,7 @@ const dApp = {
       await this.blockFitterContract.methods.unregisterAffiliateGym(parseInt(id)).send({from: this.accounts[0]}).on("receipt", async (receipt) => {
         await this.render();
       });
-      
+
     } catch (e) {
       console.log(`DEBUG: dapp.deactivateAffiliateGym - error: ${JSON.stringify(e)}`);
     }
@@ -181,6 +196,7 @@ const dApp = {
     //   $(".dapp-admin").hide();
     // }
   },
+
   main: async function() {
 
     // Initialize web3
@@ -212,159 +228,3 @@ const dApp = {
 };
 
 dApp.main();
-
-    // this.marsJson = await (await fetch("./MartianMarket.json")).json();
-    // this.auctionJson = await (await fetch("./MartianAuction.json")).json();
-
-    // this.marsContract = new window.web3.eth.Contract(
-    //   this.marsJson,
-    //   this.contractAddress,
-    //   { defaultAccount: this.accounts[0] }
-    // );
-    // console.log("Contract object", this.marsContract);
-
-    // this.isAdmin = this.accounts[0] == await this.marsContract.methods.owner().call();
-
-    // await this.updateUI();
-
-      // this.owner == await this.blockFitterContract.methods.owner().call();
-      // console.log(`DEBUG: dapp.main - 4 - ${JSON.stringify(this.accounts[0])}`);
-      // console.log("Contract object", JSON.stringify(this.blockFitterContract));
-      // console.log(`DEBUG: dapp.main - blockFitter contract: ${JSON.stringify(this.blockFitterContract)}`);
-
-
-
-
-  // updateUI: async function() {
-  //   console.log("updateUI");
-    // // refresh variables
-    // await this.collectVars();
-
-    // $("#dapp-tokens").html("");
-    // this.tokens.forEach((token) => {
-    //   try {
-    //     let endAuction = `<a token-id="${token.tokenId}" class="dapp-admin" style="display:none;" href="#" onclick="dApp.endAuction(event)">End Auction</a>`;
-    //     let bid = `<a token-id="${token.tokenId}" href="#" onclick="dApp.bid(event);">Bid</a>`;
-    //     let owner = `Owner: ${token.owner}`;
-    //     let withdraw = `<a token-id="${token.tokenId}" href="#" onclick="dApp.withdraw(event)">Withdraw</a>`
-    //     let pendingWithdraw = `Balance: ${token.pendingReturn} wei`;
-    //       $("#dapp-tokens").append(
-    //         `<div class="col m6">
-    //           <div class="card">
-    //             <div class="card-image">
-    //               <img id="dapp-image" src="https://gateway.pinata.cloud/ipfs/${token.image.replace("ipfs://", "")}">
-    //               <span id="dapp-name" class="card-title">${token.name}</span>
-    //             </div>
-    //             <div class="card-action">
-    //               <input type="number" min="${token.highestBid + 1}" name="dapp-wei" value="${token.highestBid + 1}" ${token.auctionEnded ? 'disabled' : ''}>
-    //               ${token.auctionEnded ? owner : bid}
-    //               ${token.pendingReturn > 0 ? withdraw : ''}
-    //               ${token.pendingReturn > 0 ? pendingWithdraw : ''}
-    //               ${this.isAdmin && !token.auctionEnded ? endAuction : ''}
-    //             </div>
-    //           </div>
-    //         </div>`
-    //       );
-    //   } catch (e) {
-    //     alert(JSON.stringify(e));
-    //   }
-    // });
-
-    // // hide or show admin functions based on contract ownership
-    // this.setAdmin();
-  //},
-  // bid: async function(event) {
-  //   const tokenId = $(event.target).attr("token-id");
-  //   const wei = Number($(event.target).prev().val());
-  //   await this.marsContract.methods.bid(tokenId).send({from: this.accounts[0], value: wei}).on("receipt", async (receipt) => {
-  //     M.toast({ html: "Transaction Mined! Refreshing UI..." });
-  //     await this.updateUI();
-  //   });
-  // },
-  // endAuction: async function(event) {
-  //   const tokenId = $(event.target).attr("token-id");
-  //   await this.marsContract.methods.endAuction(tokenId).send({from: this.accounts[0]}).on("receipt", async (receipt) => {
-  //     M.toast({ html: "Transaction Mined! Refreshing UI..." });
-  //     await this.updateUI();
-  //   });
-  // },
-  // withdraw: async function(event) {
-  //   const tokenId = $(event.target).attr("token-id") - 1;
-  //   await this.tokens[tokenId].auction.methods.withdraw().send({from: this.accounts[0]}).on("receipt", async (receipt) => {
-  //     M.toast({ html: "Transaction Mined! Refreshing UI..." });
-  //     await this.updateUI();
-  //   });
-  // },
-  // registerLand: async function() {
-  //   const name = $("#dapp-register-name").val();
-  //   const image = document.querySelector('input[type="file"]');
-
-  //   const pinata_api_key = $("#dapp-pinata-api-key").val();
-  //   const pinata_secret_api_key = $("#dapp-pinata-secret-api-key").val();
-
-  //   if (!pinata_api_key || !pinata_secret_api_key || !name || !image) {
-  //     M.toast({ html: "Please fill out then entire form!" });
-  //     return;
-  //   }
-
-  //   const image_data = new FormData();
-  //   image_data.append("file", image.files[0]);
-  //   image_data.append("pinataOptions", JSON.stringify({cidVersion: 1}));
-
-  //   try {
-  //     M.toast({ html: "Uploading Image to IPFS via Pinata..." });
-  //     const image_upload_response = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
-  //       method: "POST",
-  //       mode: "cors",
-  //       headers: {
-  //         pinata_api_key,
-  //         pinata_secret_api_key
-  //       },
-  //       body: image_data,
-  //     });
-
-  //     const image_hash = await image_upload_response.json();
-  //     const image_uri = `ipfs://${image_hash.IpfsHash}`;
-
-  //     M.toast({ html: `Success. Image located at ${image_uri}.` });
-  //     M.toast({ html: "Uploading JSON..." });
-
-  //     const reference_json = JSON.stringify({
-  //       pinataContent: { name, image: image_uri },
-  //       pinataOptions: {cidVersion: 1}
-  //     });
-
-  //     const json_upload_response = await fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", {
-  //       method: "POST",
-  //       mode: "cors",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         pinata_api_key,
-  //         pinata_secret_api_key
-  //       },
-  //       body: reference_json
-  //     });
-
-  //     const reference_hash = await json_upload_response.json();
-  //     const reference_uri = `ipfs://${reference_hash.IpfsHash}`;
-
-  //     M.toast({ html: `Success. Reference URI located at ${reference_uri}.` });
-  //     M.toast({ html: "Sending to blockchain..." });
-
-  //     await this.marsContract.methods.registerLand(reference_uri).send({from: this.accounts[0]}).on("receipt", async (receipt) => {
-  //       M.toast({ html: "Transaction Mined! Refreshing UI..." });
-  //       $("#dapp-register-name").val("");
-  //       $("#dapp-register-image").val("");
-  //       await this.updateUI();
-  //     });
-
-  //   } catch (e) {
-  //     alert("ERROR:", JSON.stringify(e));
-  //   }
-  // },
-
-//   try {
-
-//   } catch (e) {
-//     console.log(JSON.stringify(e));
-//   }
